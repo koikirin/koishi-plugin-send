6de00c1109be74772ca8fe8a9b43e79498c931ad
Hieuzest <girkirin@hotmail.com>
2023-07-22 09:25:53 +0800

    feat: change signature, support sandbox

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,21 @@ import { Context, Schema, Service, Fragment, SendOptions, Bot } from 'koishi'
 
 declare module 'koishi' {
   interface Context {
-    sendPrivateMessage(platform: string, channelId: string, content: Fragment, options?: SendOptions): Promise<string[]>
-    sendMessage(platform: string, channelId: string, content: Fragment, guildId?: string, options?: SendOptions): Promise<string[]>
+    sendPrivateMessage(channel: string | Send.Channel, content: Fragment, options?: SendOptions): Promise<string[]>
+    sendMessage(channel: string | Send.Channel, content: Fragment, guildId?: string, options?: SendOptions): Promise<string[]>
   }
+}
+
+function parsePlatform(channel: string | Send.Channel) {
+  let platform: string, channelId: string
+  if (typeof channel === 'string') {
+    const index = channel.indexOf(':', channel.startsWith('sandbox:') ? 8 : 0)
+    platform = channel.slice(0, index)
+    channelId = channel.slice(index + 1)
+  } else {
+    const {} = { platform, channelId } = channel
+  }
+  return [platform, channelId]
 }
 
 class Send extends Service {
@@ -15,23 +27,25 @@ class Send extends Service {
     super(ctx, '__send__', true)
   }
 
-  async sendPrivateMessage(platform: string, channelId: string, content: Fragment, options?: SendOptions) {
+  async sendPrivateMessage(channel: string | Send.Channel, content: Fragment, options?: SendOptions) {
     let bot: Bot
+    const [platform, channelId] = parsePlatform(channel)
     if (platform.includes(':'))
       bot = this.ctx.bots[platform]
-    else {
+    if (!bot) {
       bot = this.ctx.bots.find(b => b.platform === platform)
     }
     if (bot) return await bot.sendPrivateMessage(channelId, content, options)
   }
 
-  async sendMessage(platform: string, channelId: string, content: Fragment, guildId?: string, options?: SendOptions) {
+  async sendMessage(channel: string | Send.Channel, content: Fragment, guildId?: string, options?: SendOptions) {
     let bot: Bot
+    const [platform, channelId] = parsePlatform(channel)
     if (platform.includes(':'))
       bot = this.ctx.bots[platform]
-    else {
+    if (!bot) {
       const { assignee } = (await this.ctx.database.getChannel(platform, channelId, ['assignee'])) || {}
-      bot = this.ctx.bots[`${platform}:${assignee}`]
+      bot ||= this.ctx.bots[`${platform}:${assignee}`]
       bot ||= this.ctx.bots.find(b => b.platform === platform)
     }
     if (bot) return await bot.sendMessage(channelId, content, guildId, options)
@@ -44,6 +58,10 @@ namespace Send {
 
   export const Config: Schema<Config> = Schema.object({})
 
+  export interface Channel {
+    platform: string
+    channelId: string
+  }
 }
 
 Context.service('__send__', Send)
